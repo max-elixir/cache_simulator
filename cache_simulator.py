c04740189081987d7e257dc4adf5836e045ca4ee
max-elixir <maximilianguzman@gmail.com>
2018-11-20 15:17:05 -0600

    restructured. adds to empty cache and nothing else

--- cache_simulator.py
+++ cache_simulator.py
@@ -1,27 +1,26 @@
 import sys
+from lru import cache_find
 
-associtivity = 0
-ways = 16 # size of page/frame table
-size = 10 # number of offset bits
-access_time = 1
-cache = {}
-pf = 0
-ct = 0
-    
+access_time = 1                                                 # time counter for access
+page_fault = 0                                                  # counter for number of times address isn't in cache
+cache = {}                                                      # 16-way cache
+cache_valid = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  # 16-way cache's valid entries
+counter = 0                                                     # index for cache_valid
+
+# open the first parameter as a file, reach each line and find its virtual address
+# simulating using the address by seeing if it is already a valid entry in the cache
+# keep count of the number of times it isn't within the cache (cache miss)
+# if it isn't in the cache, which will have a limited size of 16 ways
+#   then give it an entry
 with open(sys.argv[1], 'r') as cache_file:
     for line in cache_file:
         x = line.split()
-        x[0] = x[0][:-1]
-        if access_time > 16:
-            access_time = 1
-        if cache.get(access_time, "nope") == "nope":
-            pf += 1
-            print("oh shoot my DOOD")
-        cache[access_time] = x[2]
-        print(x[0], x[1], x[2], access_time)
+        pc = x[0][:-1]      # program counter (PC)
+        rw = x[1]           # read/write instruction
+        va = x[2]           # virtual address
 
-        ct += 1
+        page_fault = cache_find(cache, cache_valid, va, page_fault, access_time)
+
         access_time += 1
 
-print(pf/ct * 100)
-
+print("Cache miss rate:", page_fault/(access_time-1) * 100, "%")
