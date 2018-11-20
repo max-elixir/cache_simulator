91bcd5e11b62099cbc7c165efa72bb6ac6e45038
max-elixir <maximilianguzman@gmail.com>
2018-11-20 15:15:51 -0600

    restructured. adds to empty cache and nothing else

--- lru.py
+++ lru.py
@@ -1,3 +1,11 @@
 # pass in the cache dictionary
 # if the address is within cache, its a hit
 # otherwise, lru will determine which entry in cache to use
+
+
+def cache_find(cache, cache_valid, va, pf, at):
+    if cache.get(va, "NF") == "NF":
+        pf += 1
+        cache[va] = at
+
+    return pf
