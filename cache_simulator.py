9633c2d1d805544a6df41386ea00ae1587aa03af
max-elixir <maximilianguzman@gmail.com>
2018-11-20 09:02:21 -0600

    added empty lru.py. added bad miss rate calculation.

--- cache_simulator.py
+++ cache_simulator.py
@@ -1,19 +1,27 @@
 import sys
-import numpy as np
-import pandas as pd
 
-def processCacheFile():
-    associtivity = 0
-    ways = 16 # size of page/frame table
-    size = 10 # number of offset bits
-    access_time = 1
+associtivity = 0
+ways = 16 # size of page/frame table
+size = 10 # number of offset bits
+access_time = 1
+cache = {}
+pf = 0
+ct = 0
     
-    with open(sys.argv[1], 'r') as cache_file:
-        for line in cache_file:
-            x = line.split()
-            x[0] = x[0][:-1]
-            print(x[0], x[1], x[2], access_time)
-            
-            access_time += 1
-            
-processCacheFile()
+with open(sys.argv[1], 'r') as cache_file:
+    for line in cache_file:
+        x = line.split()
+        x[0] = x[0][:-1]
+        if access_time > 16:
+            access_time = 1
+        if cache.get(access_time, "nope") == "nope":
+            pf += 1
+            print("oh shoot my DOOD")
+        cache[access_time] = x[2]
+        print(x[0], x[1], x[2], access_time)
+
+        ct += 1
+        access_time += 1
+
+print(pf/ct * 100)
+
---
+++ memory_traces/lru.py
@@ -0,0 +1,3 @@
+# pass in the cache dictionary
+# if the address is within cache, its a hit
+# otherwise, lru will determine which entry in cache to use
