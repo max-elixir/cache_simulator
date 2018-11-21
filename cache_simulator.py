8ba29d8dd4a0ebc6366a30836683ae437e763f82
max-elixir <maximilianguzman@gmail.com>
2018-11-21 09:45:03 -0600

    added comments for entire project design, from Dr. Wang

--- cache_simulator.py
+++ cache_simulator.py
@@ -6,19 +6,23 @@ page_fault = 0                                                  # counter for nu
 cache = {}                                                      # 16-way cache
 cache_valid = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  # 16-way cache's valid entries
 counter = 0                                                     # index for cache_valid
+# data structure to represent 16 way cache, add another for the sets
 
 # open the first parameter as a file, reach each line and find its virtual address
-# simulating using the address by seeing if it is already a valid entry in the cache
+# simulate using the address by seeing if it is already a valid entry in the cache
 # keep count of the number of times it isn't within the cache (cache miss)
 # if it isn't in the cache, which will have a limited size of 16 ways
 #   then give it an entry
 with open(sys.argv[1], 'r') as cache_file:
     for line in cache_file:
         x = line.split()
-        pc = x[0][:-1]      # program counter (PC)
-        rw = x[1]           # read/write instruction
-        va = x[2]           # virtual address
+        pc = x[0][:-1]          # program counter (PC), USELESS
+        rw = x[1]               # read/write instruction, USEFUL FOR A WRITE BACK-POLICY
+        va = x[2]               # virtual address, CONVERT TO INTEGER
+        dc = str(int(va, 16))   # decimal value of virtual address, USEFUL SOMEHOW?
 
+        # address is assumed 64-bit, 6 is offset, # of bits = set index,
+        # the rest is the tag
         page_fault = cache_find(cache, cache_valid, va, page_fault, access_time)
 
         access_time += 1
--- lru.py
+++ lru.py
@@ -2,10 +2,33 @@
 # if the address is within cache, its a hit
 # otherwise, lru will determine which entry in cache to use
 
+#               # # of sets
+#               # cache size
+CL_SIZE = 64    # cache line will be 64 bytes
+#               # replacement policy
+
+# def access(rw, address)
+# determine hit or miss
+#   check if in cache
+#   cut the bits to find the set id and offset in cache
+#   search in set: tag comparison
+# do hit or miss process
+#   ***if hit, update access/age/timestamp/at/access_time of Hit_CL
+#   queue uses time_stamp, can use tag or id
+#   ***if miss, find slot.
+#   use an empty way, if there is one, or use an lru
+#   update the slot
+#       set valid bit to 1
+#       update tag
+#       update timestamp
+# keep track of misses to report miss rate
+
 
 def cache_find(cache, cache_valid, va, pf, at):
     if cache.get(va, "NF") == "NF":
         pf += 1
         cache[va] = at
+    else:
+        pf = pf
 
     return pf
